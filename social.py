c18b3175f72362f78e163a8f7a798f842be59389
HeisenBerg? <admin@tiktokastudio.com>
2024-12-25 08:50:00 +0800

    Update social.py

--- social.py
+++ social.py
@@ -333,7 +333,7 @@ async def main():
         {'crazygames':"https://crazygames.com/"}
     # Add other links as needed
 ]
-domain=env_vars['DOMAIN'].lower()
+    domain=env_vars['DOMAIN'].lower()
 
 # Print all links
     for link in links:
