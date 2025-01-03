8625a6b93a543713aa22ffa477e1bc58276b9c57
HeisenBerg? <admin@tiktokastudio.com>
2025-01-03 10:04:34 +0800

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -1,1 +1,13 @@
 #https://replicate.com/recraft-ai/recraft-20b
+#parse sitemap 
+#https://replicate.com/sitemap.xml
+
+
+#https://replicate.com/sitemap-collections.xml
+# 获取类别
+
+# https://replicate.com/sitemap-models.xml
+# 获取模型url
+# <loc>https://replicate.com/processone1/evelyn-oberleitner-pictures</loc>
+#解析调用次数
+
