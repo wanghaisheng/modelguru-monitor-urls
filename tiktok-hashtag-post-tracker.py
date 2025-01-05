537494e9a640a6cbd60e166607571bc26751b0ed
HeisenBerg? <admin@tiktokastudio.com>
2025-01-05 09:08:20 +0800

    Create tiktok-hashtag-post-tracker.py

---
+++ tiktok-hashtag-post-tracker.py
@@ -0,0 +1,21 @@
+#squidgame
+https://www.tiktok.com/tag/squidgame
+find all potential related hashtag
+option1 :
+
+use google search 
+
+squid site:tiktok.com/tag/
+
+
+option 2:
+
+start from https://www.tiktok.com/tag/squidgame
+
+grab top 100 video ,collect all tags and unique ones
+
+
+
+
+
+get post count of each 
