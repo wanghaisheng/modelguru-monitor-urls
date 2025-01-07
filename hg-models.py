9b233bb44d4ce1ac3bf5ba90294951f93466bd8e
HeisenBerg? <admin@tiktokastudio.com>
2025-01-07 19:22:59 +0800

    Update hg-models.py

--- hg-models.py
+++ hg-models.py
@@ -262,7 +262,7 @@ async def main():
             file_path = 'hg.txt'
             items=exact_url_timestamp(
                 baseUrl,
-                max_count=5000,
+                max_count=5000000,
                 start_date=int(start_date.strftime('%Y%m%d')),
                 end_date=int(current_date.strftime('%Y%m%d')),
                 
